e613e358aab142f8a25f9e86fdc33fc2e8535e60
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-24 18:53:08 +0530

    it is an udemy exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(SharedDigit.hasSharedDigit(22,21));
+        System.out.println(SharedDigit.hasSharedDigit(42,21));
+        System.out.println(SharedDigit.hasSharedDigit(-22,99));
+        System.out.println(SharedDigit.hasSharedDigit(28,82));
     }
 }
--- src/SharedDigit.java
+++ src/SharedDigit.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class SharedDigit {
+public class SharedDigit {
+    public static boolean hasSharedDigit(int a, int b) {
+        if ((a > 10 && a < 100) && (b > 10 && b < 100)) {
+            if ((a / 10 == b / 10) || (a % 10 == b / 10) || (a % 10 == b % 10))
+                return true;
+            else
+                return false;
+        } else
+            return false;
+    }
 }
